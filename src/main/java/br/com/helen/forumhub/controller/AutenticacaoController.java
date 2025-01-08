144403b4039bb91154d552886f4644b44c1a5dfc
sunHelen12 <sunrise.developer12@gmail.com>
2025-01-08 18:25:10 -0300

    Controller de Autenticação  de login

---
+++ src/main/java/br/com/helen/forumhub/controller/AutenticacaoController.java
@@ -0,0 +1,35 @@
+package br.com.helen.forumhub.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.helen.forumhub.domain.usuario.Usuario;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+    
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+    
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+}
