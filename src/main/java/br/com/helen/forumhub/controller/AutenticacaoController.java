67fdf6a2b8cfc37d947a3d961c65237a377affe0
sunHelen12 <sunrise.developer12@gmail.com>
2025-01-08 20:56:01 -0300

    Atualizando classes

--- src/main/java/br/com/helen/forumhub/controller/AutenticacaoController.java
+++ src/main/java/br/com/helen/forumhub/controller/AutenticacaoController.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.helen.forumhub.domain.usuario.Usuario;
+import br.com.helen.forumhub.domain.usuario.record.DadosAutenticacao;
+import br.com.helen.forumhub.infra.security.TokenService;
+import br.com.helen.forumhub.infra.security.record.DadosTokenJWT;
 import jakarta.validation.Valid;
 
 @RestController
--- src/main/java/br/com/helen/forumhub/infra/security/record/DadosTokenJWT.java
+++ src/main/java/br/com/helen/forumhub/infra/security/record/DadosTokenJWT.java
@@ -1,5 +1,5 @@
 package br.com.helen.forumhub.infra.security.record;
 
-public record DadosToken(String token) {
+public record DadosTokenJWT(String token) {
     
 }
